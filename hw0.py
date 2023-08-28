7aa7ce8ba618d3db8467e2a5ede85d83d615434f
Coleisgay45 <63007624+Coleisgay45@users.noreply.github.com>
2023-08-28 12:24:24 -0600

    Add files via upload

---
+++ hw0.py
@@ -0,0 +1,57 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "4df06320-c4ea-4609-bb63-dae77410fc2e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "28\n"
+     ]
+    }
+   ],
+   "source": [
+    "#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD\n",
+    "class Exam:\n",
+    "    def PassFail(self, exam1, exam2, exam3, exam4):\n",
+    "        final = exam1 + exam2 + exam3 + exam4\n",
+    "        test = 600 * 0.55\n",
+    "        final = test - final\n",
+    "        return int(final)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6e688072-b837-4747-b29a-37faceb9d74e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
